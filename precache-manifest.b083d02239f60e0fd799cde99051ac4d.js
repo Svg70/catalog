535f8485dbed9c2bc681f1a971eb768d2bcdd17d
George <collectionpiter@gmail.com>
2020-02-11 09:00:36 +0300

    Updates

---
+++ precache-manifest.b083d02239f60e0fd799cde99051ac4d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be238471da5987512bee16a9c4a1daaf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83791e8102eaa0704d88",
+    "url": "/static/css/2.2a3957d1.chunk.css"
+  },
+  {
+    "revision": "d91082eb3882376bac12",
+    "url": "/static/css/main.5cc1262b.chunk.css"
+  },
+  {
+    "revision": "83791e8102eaa0704d88",
+    "url": "/static/js/2.6b09a6af.chunk.js"
+  },
+  {
+    "revision": "1f4bc554d12e1379d37358d3f7d07de0",
+    "url": "/static/js/2.6b09a6af.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d91082eb3882376bac12",
+    "url": "/static/js/main.45aaf457.chunk.js"
+  },
+  {
+    "revision": "561a6b040182ee33497b",
+    "url": "/static/js/runtime-main.94da08d7.js"
+  },
+  {
+    "revision": "0a05b7af763e2598e3570fa5822ec343",
+    "url": "/static/media/scale.0a05b7af.png"
+  }
+]);
